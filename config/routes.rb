9fcb5a296e8ae9f025ea08090357bc618e672764
binpatel31 <43681838+binpatel31@users.noreply.github.com>
2019-09-28 23:19:17 -0400

    Student, Librarian, Books, upto student checkout

---
+++ config/routes.rb
@@ -0,0 +1,37 @@
+Rails.application.routes.draw do
+  resources :admins
+  get 'sessions/new'
+  resources :logins
+  resources :students do
+  collection do
+    get 'mybooks'
+    get 'allbooks'
+    get 'logout'
+  end
+end
+  resources :users
+  resource :session
+  resources :sign_up
+
+  resources :librarians
+  resources :books
+  resource :libraries
+  resources :librarians do
+    collection do
+      get 'editlib'
+    end
+  end
+  resources :books do
+  collection do
+    get 'checkout'
+  end
+end
+
+  root 'logins#new'
+  get 'students/alllibs' => 'students#alllibs'
+  get 'students/viewbooks' => 'students#allbooks'
+  get 'students/bookmarks' => 'students#bookmark'
+  get 'students/fines' => 'students#fines'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/schema.rb
@@ -0,0 +1,97 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_09_29_014318) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "ISBN"
+    t.string "title", limit: 200
+    t.string "author", limit: 70
+    t.string "language", limit: 20
+    t.string "published", limit: 4
+    t.string "edition", limit: 4
+    t.string "library", limit: 10
+    t.string "image", limit: 500
+    t.string "subject", limit: 100
+    t.string "summary", limit: 1000
+    t.boolean "specialcollection"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "copies", limit: 6
+    t.index ["ISBN"], name: "index_books_on_ISBN", unique: true
+  end
+
+  create_table "librarians", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
+    t.string "library"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_valid"
+    t.index ["email"], name: "index_librarians_on_email", unique: true
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string "name", limit: 200
+    t.string "university", limit: 100
+    t.string "location", limit: 300
+    t.string "max_days", limit: 10
+    t.string "fines", limit: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.string "educational_level"
+    t.string "university"
+    t.string "maximum_book_limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "student_id"
+    t.string "ISBN"
+    t.string "bookname"
+    t.datetime "checkout_date"
+    t.datetime "expected_date"
+    t.datetime "return_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string "name", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_universities_on_name", unique: true
+  end
+
+end
